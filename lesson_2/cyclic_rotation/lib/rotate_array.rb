0cc199fad7eb22cd92af3f57cbc540a34bbcebf6
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-27 19:28:45 +0100

    Test: 2 element array tested

--- lesson_2/cyclic_rotation/lib/rotate_array.rb
+++ lesson_2/cyclic_rotation/lib/rotate_array.rb
@@ -1,3 +1,7 @@
 def rotate_array(rotation, array)
+  rotation.times do
+    array.unshift(array[array.length-1])
+    array.pop()
+  end
   return array
 end 
--- lesson_2/cyclic_rotation/spec/rotate_array_spec.rb
+++ lesson_2/cyclic_rotation/spec/rotate_array_spec.rb
@@ -3,5 +3,9 @@ require 'rotate_array'
 describe '#rotate_array' do
   it('takes an empty array and returns it') do 
     expect(rotate_array(0,[])).to eq([])
+  end
+
+  it('takes an array and rotates it 1 times') do
+    expect(rotate_array(1, [2, 3])).to eq([3, 2])
   end 
 end 
