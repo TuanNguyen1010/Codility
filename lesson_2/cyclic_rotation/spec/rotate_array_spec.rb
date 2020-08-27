640241d8d09418bb4ad55a7caea223eac84d1b81
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-27 19:41:32 +0100

    Test: rotates up to 4 times

--- lesson_2/cyclic_rotation/spec/rotate_array_spec.rb
+++ lesson_2/cyclic_rotation/spec/rotate_array_spec.rb
@@ -7,5 +7,13 @@ describe '#rotate_array' do
 
   it('takes an array and rotates it 1 times') do
     expect(rotate_array(1, [2, 3])).to eq([3, 2])
+  end
+
+  it('takes an array and rotates it 3 times') do 
+    expect(rotate_array(3, [3, 8, 9, 7, 6])).to eq([9, 7, 6, 3, 8])
   end 
+
+  it('takes an array and rotates it 4 times') do 
+    expect(rotate_array(4, [1, 2, 3, 4])).to eq([1, 2, 3, 4])
+  end
 end 
