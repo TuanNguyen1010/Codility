5610f8baa8433ee407437ecc7890e84082fbc173
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-27 20:42:28 +0100

    Test: arrays of different sizes

--- lesson_2/odd_arrays/lib/find_non_match.rb
+++ lesson_2/odd_arrays/lib/find_non_match.rb
@@ -1,3 +1,11 @@
-def find_non_match(array) 
-  return 1
+def find_non_match(a) 
+  hash = {}
+  a.each_with_index {|element, index|
+  new_array = a
+  new_array[index] = false
+  if new_array.include?(element) == false
+    return element
+  end
+  new_array[index] = element
+  }
 end
--- lesson_2/odd_arrays/spec/find_non_match_spec.rb
+++ lesson_2/odd_arrays/spec/find_non_match_spec.rb
@@ -4,4 +4,12 @@ describe '#find_non_match' do
   it 'takes array with 3 element and returns single non matching number' do 
     expect(find_non_match([0, 1, 0])).to eq(1)
   end
+
+  it 'takes array with 5 element and returns single non matching number' do
+    expect(find_non_match([6, 9, 4, 6, 9])).to eq(4)
+  end 
+
+  it 'takes array with 7 elenents and returns single non matching number' do 
+    expect(find_non_match([9, 3, 9, 3, 9, 7, 9])).to eq(7)
+  end 
 end 
